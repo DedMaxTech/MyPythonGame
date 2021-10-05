c0cf909db0eb861a6b577d746d95800e83d86876
MaxGyverTech <maxgyvertech@gmail.com>
2021-10-05 21:13:09 +0700

    before img change

--- game/player.py
+++ game/player.py
@@ -7,7 +7,35 @@ from game.core import Actor
 
 import cfg
 
+PLAYER_IMG = pg.image.load('game/content/player2/player.png')
+PLAYER_LEGS_IDLE = pg.image.load('game/content/player2/legs/idle.png')
+PLAYER_ARMS = pg.image.load('game/content/player2/arms.png')
+PLAYER_LEGS_AIR = pg.image.load('game/content/player/legs/air.png')
+PLAYER_LEGS_L = pg.image.load('game/content/player/legs/left.png')
+PLAYER_LEGS_R = pg.image.load('game/content/player/legs/right.png')
+BULLET_IMG = pg.image.load('game/content/player/guns/bullet.png')
 
+PLAYER_ACCELERATION = 5
+PLAYER_MAX_SPEED = 5
+JUMP_FORCE = 12
+GRAVITY = 0.4
+
+GUNS = {
+    'rifle': {'img': pg.image.load('game/content/player/guns/rifle.png'),
+              'hold_img': 0,
+              'pos': (29, 29),
+              'bull_pos': (0, 0),
+              'speed': 30,
+              'mag': 30,
+              'auto': True},
+    'pistol': {'img': pg.image.load('game/content/player2/guns/pistol.png'),
+               'hold_img': 0,
+               'pos': (29, 29),
+               'bull_pos': (0, 0),
+               'speed': 30,
+               'mag': 10,
+               'auto': True},
+}
 
 
 class Bullet():
